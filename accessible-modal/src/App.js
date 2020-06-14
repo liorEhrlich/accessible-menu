b32e5823016ad7b2b0e9b197367a6682ed4aa9bd
Lior Ehrlich <liorehrlich@gmail.com>
2020-06-14 18:32:59 +0300

    call menu.js frpm app.js

--- accessible-modal/src/App.js
+++ accessible-modal/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Menu from "./Menu";
 
 function App() {
   return (
@@ -10,6 +11,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Menu
+        header='menu'
+          menuItems={["add", "edit", "save", "delete"]}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
